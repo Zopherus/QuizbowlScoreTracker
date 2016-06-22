a89b0db89792dabeeeb3d9dec80d9cd29d630198
Eric Zhu <ejz1999@yahoo.com>
2016-06-22 08:47:22 -0700

    Initialized score keeping form

---
+++ QuizbowlScoreTracker/ScoreKeeping.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace QuizbowlScoreTracker
+{
+    partial class ScoreKeeping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.teamName1textBox = new System.Windows.Forms.TextBox();
+            this.TeamName1Label = new System.Windows.Forms.Label();
+            this.TeamName2Label = new System.Windows.Forms.Label();
+            this.TeamName2textBox = new System.Windows.Forms.TextBox();
+            this.PlayerNamesLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.Team1ScoreLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Team1NegButton = new System.Windows.Forms.Button();
+            this.Team1AnswerButton = new System.Windows.Forms.Button();
+            this.Team1PowerButton = new System.Windows.Forms.Button();
+            this.Team2PowerButton = new System.Windows.Forms.Button();
+            this.Team2AnswerButton = new System.Windows.Forms.Button();
+            this.Team2NegButton = new System.Windows.Forms.Button();
+            this.NextQuestionButton = new System.Windows.Forms.Button();
+            this.QuestionNumberLabel = new System.Windows.Forms.Label();
+            this.EndGameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // teamName1textBox
+            // 
+            this.teamName1textBox.Location = new System.Drawing.Point(184, 27);
+            this.teamName1textBox.Name = "teamName1textBox";
+            this.teamName1textBox.Size = new System.Drawing.Size(100, 20);
+            this.teamName1textBox.TabIndex = 0;
+            // 
+            // TeamName1Label
+            // 
+            this.TeamName1Label.AutoSize = true;
+            this.TeamName1Label.Location = new System.Drawing.Point(110, 27);
+            this.TeamName1Label.Name = "TeamName1Label";
+            this.TeamName1Label.Size = new System.Drawing.Size(68, 13);
+            this.TeamName1Label.TabIndex = 1;
+            this.TeamName1Label.Text = "Team Name:";
+            // 
+            // TeamName2Label
+            // 
+            this.TeamName2Label.AutoSize = true;
+            this.TeamName2Label.Location = new System.Drawing.Point(428, 24);
+            this.TeamName2Label.Name = "TeamName2Label";
+            this.TeamName2Label.Size = new System.Drawing.Size(68, 13);
+            this.TeamName2Label.TabIndex = 3;
+            this.TeamName2Label.Text = "Team Name:";
+            // 
+            // TeamName2textBox
+            // 
+            this.TeamName2textBox.Location = new System.Drawing.Point(502, 24);
+            this.TeamName2textBox.Name = "TeamName2textBox";
+            this.TeamName2textBox.Size = new System.Drawing.Size(100, 20);
+            this.TeamName2textBox.TabIndex = 2;
+            // 
+            // PlayerNamesLabel
+            // 
+            this.PlayerNamesLabel.AutoSize = true;
+            this.PlayerNamesLabel.Location = new System.Drawing.Point(28, 80);
+            this.PlayerNamesLabel.Name = "PlayerNamesLabel";
+            this.PlayerNamesLabel.Size = new System.Drawing.Size(75, 13);
+            this.PlayerNamesLabel.TabIndex = 4;
+            this.PlayerNamesLabel.Text = "Player Names:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(265, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(337, 84);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(421, 84);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(75, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(518, 84);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(75, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(612, 84);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(75, 20);
+            this.textBox7.TabIndex = 11;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(693, 84);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(75, 20);
+            this.textBox8.TabIndex = 12;
+            // 
+            // Team1ScoreLabel
+            // 
+            this.Team1ScoreLabel.AutoSize = true;
+            this.Team1ScoreLabel.Location = new System.Drawing.Point(128, 196);
+            this.Team1ScoreLabel.Name = "Team1ScoreLabel";
+            this.Team1ScoreLabel.Size = new System.Drawing.Size(68, 13);
+            this.Team1ScoreLabel.TabIndex = 13;
+            this.Team1ScoreLabel.Text = "Total Score: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Total Score: ";
+            // 
+            // Team1NegButton
+            // 
+            this.Team1NegButton.Location = new System.Drawing.Point(93, 246);
+            this.Team1NegButton.Name = "Team1NegButton";
+            this.Team1NegButton.Size = new System.Drawing.Size(75, 23);
+            this.Team1NegButton.TabIndex = 15;
+            this.Team1NegButton.Text = "Neg";
+            this.Team1NegButton.UseVisualStyleBackColor = true;
+            // 
+            // Team1AnswerButton
+            // 
+            this.Team1AnswerButton.Location = new System.Drawing.Point(93, 285);
+            this.Team1AnswerButton.Name = "Team1AnswerButton";
+            this.Team1AnswerButton.Size = new System.Drawing.Size(75, 23);
+            this.Team1AnswerButton.TabIndex = 16;
+            this.Team1AnswerButton.Text = "Answer";
+            this.Team1AnswerButton.UseVisualStyleBackColor = true;
+            // 
+            // Team1PowerButton
+            // 
+            this.Team1PowerButton.Location = new System.Drawing.Point(93, 329);
+            this.Team1PowerButton.Name = "Team1PowerButton";
+            this.Team1PowerButton.Size = new System.Drawing.Size(75, 23);
+            this.Team1PowerButton.TabIndex = 17;
+            this.Team1PowerButton.Text = "Power";
+            this.Team1PowerButton.UseVisualStyleBackColor = true;
+            // 
+            // Team2PowerButton
+            // 
+            this.Team2PowerButton.Location = new System.Drawing.Point(471, 335);
+            this.Team2PowerButton.Name = "Team2PowerButton";
+            this.Team2PowerButton.Size = new System.Drawing.Size(75, 23);
+            this.Team2PowerButton.TabIndex = 20;
+            this.Team2PowerButton.Text = "Power";
+            this.Team2PowerButton.UseVisualStyleBackColor = true;
+            // 
+            // Team2AnswerButton
+            // 
+            this.Team2AnswerButton.Location = new System.Drawing.Point(471, 285);
+            this.Team2AnswerButton.Name = "Team2AnswerButton";
+            this.Team2AnswerButton.Size = new System.Drawing.Size(75, 23);
+            this.Team2AnswerButton.TabIndex = 19;
+            this.Team2AnswerButton.Text = "Answer";
+            this.Team2AnswerButton.UseVisualStyleBackColor = true;
+            // 
+            // Team2NegButton
+            // 
+            this.Team2NegButton.Location = new System.Drawing.Point(471, 246);
+            this.Team2NegButton.Name = "Team2NegButton";
+            this.Team2NegButton.Size = new System.Drawing.Size(75, 23);
+            this.Team2NegButton.TabIndex = 18;
+            this.Team2NegButton.Text = "Neg";
+            this.Team2NegButton.UseVisualStyleBackColor = true;
+            // 
+            // NextQuestionButton
+            // 
+            this.NextQuestionButton.Location = new System.Drawing.Point(308, 438);
+            this.NextQuestionButton.Name = "NextQuestionButton";
+            this.NextQuestionButton.Size = new System.Drawing.Size(104, 23);
+            this.NextQuestionButton.TabIndex = 21;
+            this.NextQuestionButton.Text = "Next Question";
+            this.NextQuestionButton.UseVisualStyleBackColor = true;
+            // 
+            // QuestionNumberLabel
+            // 
+            this.QuestionNumberLabel.AutoSize = true;
+            this.QuestionNumberLabel.Location = new System.Drawing.Point(185, 427);
+            this.QuestionNumberLabel.Name = "QuestionNumberLabel";
+            this.QuestionNumberLabel.Size = new System.Drawing.Size(92, 13);
+            this.QuestionNumberLabel.TabIndex = 22;
+            this.QuestionNumberLabel.Text = "Question Number ";
+            // 
+            // EndGameButton
+            // 
+            this.EndGameButton.Location = new System.Drawing.Point(650, 469);
+            this.EndGameButton.Name = "EndGameButton";
+            this.EndGameButton.Size = new System.Drawing.Size(75, 23);
+            this.EndGameButton.TabIndex = 23;
+            this.EndGameButton.Text = "End Game";
+            this.EndGameButton.UseVisualStyleBackColor = true;
+            // 
+            // ScoreKeeping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 584);
+            this.Controls.Add(this.EndGameButton);
+            this.Controls.Add(this.QuestionNumberLabel);
+            this.Controls.Add(this.NextQuestionButton);
+            this.Controls.Add(this.Team2PowerButton);
+            this.Controls.Add(this.Team2AnswerButton);
+            this.Controls.Add(this.Team2NegButton);
+            this.Controls.Add(this.Team1PowerButton);
+            this.Controls.Add(this.Team1AnswerButton);
+            this.Controls.Add(this.Team1NegButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Team1ScoreLabel);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PlayerNamesLabel);
+            this.Controls.Add(this.TeamName2Label);
+            this.Controls.Add(this.TeamName2textBox);
+            this.Controls.Add(this.TeamName1Label);
+            this.Controls.Add(this.teamName1textBox);
+            this.Name = "ScoreKeeping";
+            this.Text = "s";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox teamName1textBox;
+        private System.Windows.Forms.Label TeamName1Label;
+        private System.Windows.Forms.Label TeamName2Label;
+        private System.Windows.Forms.TextBox TeamName2textBox;
+        private System.Windows.Forms.Label PlayerNamesLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label Team1ScoreLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Team1NegButton;
+        private System.Windows.Forms.Button Team1AnswerButton;
+        private System.Windows.Forms.Button Team1PowerButton;
+        private System.Windows.Forms.Button Team2PowerButton;
+        private System.Windows.Forms.Button Team2AnswerButton;
+        private System.Windows.Forms.Button Team2NegButton;
+        private System.Windows.Forms.Button NextQuestionButton;
+        private System.Windows.Forms.Label QuestionNumberLabel;
+        private System.Windows.Forms.Button EndGameButton;
+    }
+}
+
